1f4c23a3be0caa8e86350bdbfb5058d47a1fcf8b
atalha74 <121051841+atalha74@users.noreply.github.com>
2023-08-28 15:20:14 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int p1,p2,s1,s2,s3,s4,s5,s6,s7,s8,s9,s;
+    printf("Tic Tac Toe!\n");
+    printf("Please enter a name for the first player:\n");
+    scanf("%d",&p1);
+    p1= getchar();
+    printf("Please enter a name for the second player:\n");
+    scanf("%d",&p2);
+    p2= getchar();
+    printf("player one select the sign: X/O ?\n");
+    scanf("%d",&s);
+    s=getchar();
+    printf("")
+
+    }
+    return 0;
+}
